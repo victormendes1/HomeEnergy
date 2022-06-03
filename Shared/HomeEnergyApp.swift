0903bb09c817f55ed8c7a26da3babbff0b9b667b
Victor Santos <visantos@brq.com>
2022-06-02 21:55:15 -0300

    Initial Commit

---
+++ Shared/HomeEnergyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeEnergyApp.swift
+//  Shared
+//
+//  Created by Victor Mendes on 02/06/22.
+//
+
+import SwiftUI
+
+@main
+struct HomeEnergyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
